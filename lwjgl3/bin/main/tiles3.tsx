1f87028c890a3df0d93b5eb7d666fb6412b2336b
skinnyleonard <123135015+skinnyleonard@users.noreply.github.com>
2024-11-20 15:18:28 -0300

    Add files via upload

---
+++ lwjgl3/bin/main/tiles3.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="tiles3" tilewidth="64" tileheight="64" tilecount="8" columns="8">
+ <image source="tiles3.png" width="512" height="64"/>
+ <tile id="3" type="flag"/>
+</tileset>
