8a059f953eb0af20500af34d2d432998e5e56882
skinnyleonard <123135015+skinnyleonard@users.noreply.github.com>
2024-11-27 00:05:50 -0300

    Add files via upload

--- lwjgl3/bin/main/tiles3.tsx
+++ lwjgl3/bin/main/tiles3.tsx
@@ -2,4 +2,23 @@
 <tileset version="1.10" tiledversion="1.11.0" name="tiles3" tilewidth="64" tileheight="64" tilecount="8" columns="8">
  <image source="tiles3.png" width="512" height="64"/>
  <tile id="3" type="flag"/>
+ <tile id="4">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0">
+    <properties>
+     <property name="friction" type="float" value="100"/>
+    </properties>
+    <polyline points="0,0 64,0"/>
+   </object>
+   <object id="3" x="0" y="0">
+    <polyline points="0,0 0,64"/>
+   </object>
+   <object id="4" x="0" y="64">
+    <polyline points="0,0 64,0"/>
+   </object>
+   <object id="5" x="64" y="0">
+    <polyline points="0,0 0,64"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
